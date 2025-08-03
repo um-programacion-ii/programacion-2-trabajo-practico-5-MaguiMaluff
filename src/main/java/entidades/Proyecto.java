f7f83dcf47d29d288594b68febc30879ed17b55f
“MaguiMaluff” <maguimalu4@gmail.com>
2025-08-03 11:58:33 -0300

    Issue 5, anotaciones JPA agregadas

--- src/main/java/entidades/Proyecto.java
+++ src/main/java/entidades/Proyecto.java
@@ -1,6 +1,6 @@
 package entidades;
 
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +11,28 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Entity
+@Table(name = "proyectos")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String nombre;
+
+    @Column(length = 1000)
     private String descripcion;
+
+    @Column(name = "fecha_inicio")
+    @Temporal(TemporalType.DATE)
     private LocalDate fechaInicio;
+
+    @Column(name = "fecha_fin")
+    @Temporal(TemporalType.DATE)
     private LocalDate fechaFina;
 
     @ManyToMany(mappedBy = "proyectos")
